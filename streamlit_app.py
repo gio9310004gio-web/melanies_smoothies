5b69f1700d905bf33e9f327a0bce5d36510f85be
gio9310004gio-web <gio9310004gio@gmail.com>
2025-09-19 15:33:44 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@
 import streamlit as st
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col,when_matched
-
+import requests
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Youe Smoothie!:cup_with_straw: ")
 st.write(
@@ -22,15 +22,14 @@ ingredients_list= st.multiselect(
     'Choose up to 5 ingredients:', my_dataframe,max_selections=5 )
 
 if ingredients_list:
-    #st.write(ingredients_list)
-    #st.text(ingredients_list)
-    
+   
     ingredients_string=''
     
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen +' '
-
-    #st.text(ingredients_string)
+        st.subheader(fruit_chosen+'Nutrition Information')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
      
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
@@ -43,7 +42,3 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         
         st.success('Your Smoothie is ordered! , '+ name_on_order, icon="✅")
-
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
